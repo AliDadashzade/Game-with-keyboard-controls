1271a9699382c7ef94f9e3d961634c07a3c1b181
Ali Dadaszada <alidadaszada6@mail.ru>
2022-02-10 19:53:24 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Game_with_keyboard_controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(417, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Question";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(42, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "---";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1027, 34);
+            this.label3.TabIndex = 6;
+            this.label3.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(910, 34);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "  ";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(110, 495);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(918, 34);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "  ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 210);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(815, 534);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(95, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(1, 655);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1027, 34);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "  ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(85, 109);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 31);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "---";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(313, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 31);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "---";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(516, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 31);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "---";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(765, 109);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 31);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "---";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(933, 213);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(95, 118);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(4, 371);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(95, 118);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(961, 100);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 31);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "---";
+            this.label11.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(758, 522);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(188, 142);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 19;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1029, 688);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_with_keyboard_controls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int soru = 0;
+        private void dis()
+        {
+            label7.Enabled = false;
+            label8.Enabled = false;
+            label9.Enabled = false;
+            label10.Enabled = false;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            if (e.KeyCode == Keys.Right && x<700)
+            {
+                x +=10;
+            }
+            if (e.KeyCode == Keys.Left && x>0)
+            {
+                x -= 10;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                y -= 10;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                y += 10;
+            }
+            pictureBox1.Location = new Point(x, y);
+            if (pictureBox1.Left>=pictureBox3.Left)
+            {
+                soru = 1;
+                if (soru == 1)
+                {
+                    pictureBox3.Visible = false;
+                    label2.Text = "Donusum kitabinin yazari kimdir?";
+                    label7.Text = "Sabahattin Ali";
+                    label8.Text = "Franz Kafka";
+                    label9.Text = "George Orwell";
+                    label10.Text = "Tolstoy";
+                    label11.Text = "Franz Kafka";
+                }
+            }
+            if (pictureBox1.Left <= pictureBox4.Left)
+            {
+                soru++;
+                if (soru == 2)
+                {
+                    pictureBox4.Visible = false;
+                    label7.Enabled = true;
+                    label8.Enabled = true;
+                    label9.Enabled = true;
+                    label10.Enabled = true;
+                    label7.BackColor = Color.Black;
+                    label8.BackColor = Color.Black;
+                    label9.BackColor = Color.Black;
+                    label10.BackColor = Color.Black;
+                    label2.Text = "1984 kitabinin yazari kimdir?";
+                    label7.Text = "Dostoyevski";
+                    label8.Text = "S.Zweig";
+                    label9.Text = "George Orwell";
+                    label10.Text = "Tolstoy";
+                    label11.Text = "George Orwell";
+                }
+            }
+            if(pictureBox1.Left>=pictureBox2.Left && pictureBox1.Top >=pictureBox2.Top)
+            {
+                pictureBox5.Visible = true;
+                pictureBox1.Visible =false;
+                pictureBox2.Visible = false;
+                MessageBox.Show("You won!");
+            }
+        } 
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (label11.Text == label7.Text)
+            {
+                label7.BackColor = Color.Green;
+            }
+            else
+            {
+                label7.BackColor=Color.Red;
+            }
+            dis();
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (label11.Text == label8.Text)
+            {
+                label8.BackColor = Color.Green;
+            }
+            else
+            {
+                label8.BackColor = Color.Red;
+            }
+            dis();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (label11.Text == label9.Text)
+            {
+                label9.BackColor = Color.Green;
+            }
+            else
+            {
+                label9.BackColor = Color.Red;
+            }
+            dis();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            if (label11.Text == label10.Text)
+            {
+                label10.BackColor = Color.Green;
+            }
+            else
+            {
+                label10.BackColor = Color.Red;
+            }
+            dis();
+        }
+    }
+}
